4595930b8ef0ba1e40dfd37fa883d4f90233f573
Ricardo Melo <ricardofrontend@gmail.com>
2022-01-04 08:38:32 -0300

    update

--- src/components/TaskList/tasklist.js
+++ src/components/TaskList/tasklist.js
@@ -3,7 +3,19 @@ import './tasklist.css';
 export const TaskList = () => {
     return (
         <div className='tasklist-wrapper'>
-            <div>...</div>
+            <div className='tl-container'>...</div>
+            <div className='tl-container'>...</div>
+            <div className='tl-container'>
+                <div className='tl-desc'>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </div>
+                <div className='tl-title'>
+                    <input type="checkbox" className='tl-checkbox' name="vehicle1"/>
+                    <h3>Comprar pão no supermercado</h3>
+                    <div className='tl-arrow'>🔽</div>
+                </div>
+                <div className='tl-date'>03-01-2022</div>
+                <div className='tl-btn'>✏️</div>
+                <div className='tl-btn'>🗑️</div>
+            </div>
         </div>
     )
 }
